45720a6a6300010a89c1ed174ce2e3c369b547e7
Georgi Hristov <georgi.dh@gmail.com>
2023-01-10 23:33:01 +0200

    Add two database models.

---
+++ MyDreamHomeApp/Data/Models/Home.cs
@@ -0,0 +1,17 @@
+﻿namespace MyDreamHomeApp.Data.Models
+{
+    public class Home
+    {
+        public int Id { get; init; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Drawing { get; set; }
+
+        public string Type { get; set; }
+
+        public string Address { get; set; }
+
+        public IEnumerable<Room> Rooms { get; set; }
+
+    }
+}
---
+++ MyDreamHomeApp/Data/Models/Room.cs
@@ -0,0 +1,19 @@
+﻿namespace MyDreamHomeApp.Data.Models
+{
+    public class Room
+    {
+        public int Id { get; init; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public decimal Area { get; set; }
+        public string FloоrType { get; set; }
+
+        public IEnumerable<Matirial> Matirials { get; set; }
+
+        public IEnumerable<Equipment> Equipments { get; set; }
+
+        public IEnumerable<Furniture> Furniture { get; set; }
+
+        public IEnumerable<Idea> Ideas { get; set; }
+    }
+}
